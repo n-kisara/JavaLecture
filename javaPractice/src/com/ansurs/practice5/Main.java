727995596eb3c237938dd92b1784eb78ef4b50c6
n-kisara <136792599+n-kisara@users.noreply.github.com>
2024-05-28 14:19:20 +0900

    Update Main.java

--- javaPractice/src/com/ansurs/practice5/Main.java
+++ javaPractice/src/com/ansurs/practice5/Main.java
@@ -1,7 +1,5 @@
 package com.ansurs.practice5;
 
-import javax.lang.model.element.Name;
-
 public class Main {
     public static void ShowProfile(String Name, int Age) {
         System.out.println("名前は" + Name + "、" + Age + "歳です。");
